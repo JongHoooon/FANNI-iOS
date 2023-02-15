f09403555d2908591e85821a615eabb4d89ee438
JongHooon <qnm541@gmail.com>
2023-02-16 02:28:09 +0900

    feat: add icon

---
+++ FANNI-iOS/Presentation/AuthScene/Auth/Reactor/AuthViewReactor.swift
@@ -0,0 +1,99 @@
+//
+//  AuthViewReactor.swift
+//  FANNI-iOS
+//
+//  Created by JongHoon on 2023/02/12.
+//
+
+import ReactorKit
+import RxSwift
+
+import KakaoSDKAuth
+import RxKakaoSDKAuth
+import KakaoSDKUser
+import RxKakaoSDKUser
+
+final class AuthReactor: Reactor {
+    
+    private let disposeBag = DisposeBag()
+    
+    enum Action {
+        case tapKakaoLogin
+    }
+    
+    enum Mutation {
+    }
+    
+    struct State {
+    }
+    
+    let initialState = State()
+    
+    init() {
+        
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .tapKakaoLogin:
+            loginWithKakao()
+            return .empty()
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
+
+// MARK: - Private Method
+private extension AuthReactor {
+    func loginWithKakao() {
+        
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.rx.loginWithKakaoTalk()
+                .subscribe(onNext: { [weak self] _ in
+                    print("\(Date().toString()) [✨][\(#fileID), \(#line), \(#function)] - Login With KakaoTalk success.")
+                    
+                    self?.getInfoWithKakao()
+                    NotificationCenter.default.post(name: .loginSuccess, object: nil)
+                }, onError: { error in
+                    print("\(Date().toString()) [🚨][\(#fileID), \(#line), \(#function)] - login With KakaoTalk error: \(error)")
+                })
+                .disposed(by: disposeBag)
+        } else {
+            UserApi.shared.rx.loginWithKakaoAccount()
+                .subscribe(onNext: { [weak self] _ in
+                    print("\(Date().toString()) [✨][\(#fileID), \(#line), \(#function)] - Login With KakaoAccount sucess.")
+                    
+                    self?.getInfoWithKakao()
+                    NotificationCenter.default.post(name: .loginSuccess, object: nil)
+                }, onError: { error in
+                    print("\(Date().toString()) [🚨][\(#fileID), \(#line), \(#function)] - login With KakaoAccount error: \(error)")
+                })
+                .disposed(by: disposeBag)
+        }
+    }
+    
+    func getInfoWithKakao() {
+        UserApi.shared.rx.me()
+            .subscribe(onSuccess: { user in
+                
+                let id: Int = Int(user.id ?? 0)
+                let nickName: String = user.kakaoAccount?.profile?.nickname ?? ""
+                let email: String = user.kakaoAccount?.email ?? ""
+                let birthDay: String = user.kakaoAccount?.birthday ?? ""
+                
+                UserManager.kakaoID = id
+                UserManager.kakaoNickname = nickName
+                UserManager.kakaoEmail = email
+                UserManager.birthday = birthDay
+                
+                print("\(Date().toString()) [✨][\(#fileID), \(#line), \(#function)] - Kakao ID: \(id), Kakao Nickname: \(nickName), email: \(email), birthday: \(birthDay)")
+                
+            }, onFailure: { error in
+                print("\(Date().toString()) [🚨][\(#fileID), \(#line), \(#function)] - Get info with kakao error: \(error)")
+            })
+            .disposed(by: disposeBag)
+    }
+}
