f6f486979b6ed057d5a2dac8aeed9477678fcd28
JongHooon <qnm541@gmail.com>
2023-02-20 14:39:37 +0900

    feat: add second onboarding reactor

--- FANNI-iOS/Presentation/AuthScene/SecondOnboarding/Reactor/SecondOnboardingReactor.swift
+++ FANNI-iOS/Presentation/AuthScene/SecondOnboarding/Reactor/SecondOnboardingReactor.swift
@@ -12,15 +12,18 @@ import RxSwift
 final class SecondOnboardingReactor: Reactor {
     
     enum Action {
-        
+        case tapLunarButton
+        case tapOtherButton
     }
     
     enum Mutation {
-        
+        case tapLunarButton
+        case tapOtherButton
     }
     
     struct State {
-        
+        var checkLunarButton: Bool = false
+        var checkOtherButton: Bool = false
     }
     
     let initialState = State()
@@ -29,11 +32,27 @@ final class SecondOnboardingReactor: Reactor {
         
     }
     
-    //  func mutate(action: Action) -> Observable<Mutation> {
-    //    <#code#>
-    //  }
-    //
-    //  func reduce(state: State, mutation: Mutation) -> State {
-    //    <#code#>
-    //  }
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            
+        case .tapLunarButton:
+            return .just(.tapLunarButton)
+        case .tapOtherButton:
+            return .just(.tapOtherButton)
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+            
+        case .tapLunarButton:
+            state.checkLunarButton.toggle()
+            state.checkOtherButton = false
+        case .tapOtherButton:
+            state.checkOtherButton.toggle()
+            state.checkLunarButton = false
+        }
+        return state
+    }
 }
--- FANNI-iOS/Presentation/AuthScene/SecondOnboarding/View/SecondOnboardingViewController.swift
+++ FANNI-iOS/Presentation/AuthScene/SecondOnboarding/View/SecondOnboardingViewController.swift
@@ -26,12 +26,57 @@ final class SecondOnboardingViewController: BaseViewController, View {
         return imageView
     }()
     
-    private lazy var infoLabel1: UILabel = {
+    private lazy var infoLabel: UILabel = {
         let label = UILabel()
         label.text = "첫 기념일로\n\(UserManager.nickName)님의 생일을\n알려주시겠어요?"
         label.font = .pretendar(weight: ._700, size: 24.0)
         label.textColor = .Font.font1
         label.numberOfLines = 3
+        return label
+    }()
+    
+    private lazy var hiddendInfoLabel1: UILabel = {
+        let label = UILabel()
+        label.text = "어떤 기념일인가요?"
+        label.font = .pretendar(weight: ._700, size: 24.0)
+        label.textColor = .Font.font1
+        label.numberOfLines = 1
+        label.isHidden = true
+        label.alpha = 0
+        return label
+    }()
+    
+    private lazy var hiddenMenuButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("기념일 선택", for: .normal)
+        button.setTitleColor(.Font.font3, for: .normal)
+        button.titleLabel?.textAlignment = .left
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.deactiveTextField.cgColor
+        button.layer.cornerRadius = 12.0
+
+        
+        button.titleLabel?.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16.0)
+           $0.trailing.equalTo(button.imageView!.snp.leading).offset(-4.0)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(112)
+        }
+        
+        button.isHidden = true
+        button.alpha = 0
+        
+        return button
+    }()
+    
+    private lazy var hiddendInfoLabel2: UILabel = {
+        let label = UILabel()
+        label.text = "기념일 날짜를 알려주세요."
+        label.font = .pretendar(weight: ._500, size: 16.0)
+        label.textColor = .Font.font2
+        label.numberOfLines = 1
+        label.isHidden = true
+        label.alpha = 0
         return label
     }()
     
@@ -72,7 +117,7 @@ final class SecondOnboardingViewController: BaseViewController, View {
         textField.textColor = .Font.font1
         textField.layer.borderWidth = 1.0
         textField.layer.cornerRadius = 12.0
-        textField.layer.borderColor = UIColor.main2.cgColor
+        textField.layer.borderColor = UIColor.deactiveTextField.cgColor
         textField.tintColor = .main1
         textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16.0, height: 56.0))
         textField.leftViewMode = .always
@@ -100,9 +145,11 @@ final class SecondOnboardingViewController: BaseViewController, View {
     private lazy var previousButton: UIButton = {
         let button = UIButton()
         button.setTitle("이전", for: .normal)
+        button.setTitleColor(.main1, for: .normal)
         button.titleLabel?.font = .pretendar(weight: ._700, size: 16.0)
-        button.titleLabel?.textColor = .white
-        button.backgroundColor = .main1
+        button.backgroundColor = .Background.background1
+        button.layer.borderColor = UIColor.main1.cgColor
+        button.layer.borderWidth = 1.0
         button.layer.cornerRadius = 12.0
         return button
     }()
@@ -110,13 +157,52 @@ final class SecondOnboardingViewController: BaseViewController, View {
     private lazy var nextButton: UIButton = {
         let button = UIButton()
         button.setTitle("다음", for: .normal)
+        button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = .pretendar(weight: ._700, size: 16.0)
-        button.titleLabel?.textColor = .white
         button.backgroundColor = .main1
         button.layer.cornerRadius = 12.0
         return button
     }()
-
+    
+    private lazy var lunarStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [lunarCheckButton, lunarLabel])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 12.0
+        
+        lunarCheckButton.snp.makeConstraints {
+            $0.height.width.equalTo(22.0)
+        }
+        lunarCheckButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        lunarLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        return stackView
+    }()
+    
+    private lazy var otherAnniversaryStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [otherAnniversaryCheckButton, otherAnniversaryLabel])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 12.0
+        
+        otherAnniversaryCheckButton.snp.makeConstraints {
+            $0.height.width.equalTo(22.0)
+        }
+        otherAnniversaryCheckButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        otherAnniversaryLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        return stackView
+    }()
+    
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [lunarStackView,
+                                                       otherAnniversaryStackView])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .leading
+        stackView.spacing = 24.0
+        return stackView
+    }()
     
     // MARK: - Life Cycle
     
@@ -124,10 +210,28 @@ final class SecondOnboardingViewController: BaseViewController, View {
         super.viewDidLoad()
         configLayout()
         configNavigationBar()
+        
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            guard let self = self else { return }
+            
+            self.infoLabel.alpha = 0
+            self.hiddendInfoLabel1.alpha = 1
+            self.hiddendInfoLabel1.isHidden = false
+            
+            self.otherAnniversaryStackView.alpha = 0
+            
+            self.hiddendInfoLabel2.alpha = 1
+            self.hiddendInfoLabel2.isHidden = false
+//            self.otherAnniversaryStackView.isHidden = true
+            
+            self.hiddenMenuButton.alpha = 1
+            self.hiddenMenuButton.isHidden = false
+            
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        anniversaryTextField.endEditing(true)
+        view.endEditing(true)
     }
     
     // MARK: - Init
@@ -148,8 +252,27 @@ extension SecondOnboardingViewController {
     
     func bind(reactor: SecondOnboardingReactor) {
         
+        lunarCheckButton.rx.tap
+            .map { Reactor.Action.tapLunarButton }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        otherAnniversaryCheckButton.rx.tap
+            .map { Reactor.Action.tapOtherButton }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
         // MARK: Action
         
+        reactor.state.asObservable().map { $0.checkLunarButton }
+            .asDriver(onErrorJustReturn: false)
+            .drive(lunarCheckButton.rx.isCheck)
+            .disposed(by: disposeBag)
+        
+        reactor.state.asObservable().map { $0.checkOtherButton }
+            .asDriver(onErrorJustReturn: false)
+            .drive(otherAnniversaryCheckButton.rx.isCheck)
+            .disposed(by: disposeBag)
     }
 }
 
@@ -169,53 +292,17 @@ private extension SecondOnboardingViewController {
     }
     
     func configLayout() {
-        let lunarStackView: UIStackView = {
-            let stackView = UIStackView(arrangedSubviews: [lunarCheckButton, lunarLabel])
-            stackView.axis = .horizontal
-            stackView.distribution = .fill
-            stackView.alignment = .fill
-            stackView.spacing = 12.0
-            
-            lunarCheckButton.snp.makeConstraints {
-                $0.height.width.equalTo(22.0)
-            }
-            lunarCheckButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-            lunarLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-            return stackView
-        }()
         
-        let otherAnniversaryStackView: UIStackView = {
-            let stackView = UIStackView(arrangedSubviews: [otherAnniversaryCheckButton, otherAnniversaryLabel])
-            stackView.axis = .horizontal
-            stackView.distribution = .fill
-            stackView.alignment = .fill
-            stackView.spacing = 12.0
-            
-            otherAnniversaryCheckButton.snp.makeConstraints {
-                $0.height.width.equalTo(22.0)
-            }
-            otherAnniversaryCheckButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-            otherAnniversaryLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-            return stackView
-        }()
-        
-        let buttonStackView: UIStackView = {
-            let stackView = UIStackView(arrangedSubviews: [lunarStackView,
-                                                           otherAnniversaryStackView])
-            stackView.axis = .vertical
-            stackView.distribution = .equalSpacing
-            stackView.alignment = .leading
-            stackView.spacing = 24.0
-            return stackView
-        }()
-
         view.backgroundColor = .systemBackground
         
         [
             progressImageView,
-            infoLabel1,
+            infoLabel,
+            hiddendInfoLabel1,
+            hiddenMenuButton,
             anniversaryTextField,
             buttonStackView,
+            previousButton,
             nextButton
             
         ].forEach { view.addSubview($0) }
@@ -225,26 +312,47 @@ private extension SecondOnboardingViewController {
             $0.centerX.equalToSuperview()
         }
     
-        infoLabel1.snp.makeConstraints {
+        infoLabel.snp.makeConstraints {
+            $0.top.equalTo(progressImageView.snp.bottom).offset(40.0)
+            $0.leading.equalToSuperview().inset(20.0)
+        }
+                
+        hiddendInfoLabel1.snp.makeConstraints {
             $0.top.equalTo(progressImageView.snp.bottom).offset(40.0)
             $0.leading.equalToSuperview().inset(20.0)
         }
         
+        hiddenMenuButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20.0)
+            $0.top.equalTo(hiddendInfoLabel1.snp.bottom).offset(40.0)
+            $0.width.equalTo(160.0)
+            $0.height.equalTo(56.0)
+        }
+        
         anniversaryTextField.snp.makeConstraints {
-            $0.top.equalTo(infoLabel1.snp.bottom).offset(40.0)
+            $0.top.equalTo(infoLabel.snp.bottom).offset(40.0)
             $0.leading.trailing.equalToSuperview().inset(20.0)
             $0.height.equalTo(56.0)
         }
         
         buttonStackView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(20.0)
-            $0.top.equalTo(anniversaryTextField.snp.bottom).offset(40.0)
+            $0.top.equalTo(anniversaryTextField.snp.bottom).offset(24.0)
+        }
+        
+        previousButton.snp.makeConstraints { [weak self] in
+            guard let self = self else { return }
+            $0.leading.equalToSuperview().inset(20.0)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-34.0)
+            $0.width.equalTo((self.view.frame.width - 20*2 - 8)/3)
+            $0.height.equalTo(56.0)
         }
         
         nextButton.snp.makeConstraints {
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16.0)
-            $0.leading.trailing.equalToSuperview().inset(20.0)
-            $0.height.equalTo(44.0)
+            $0.leading.equalTo(previousButton.snp.trailing).offset(8.0)
+            $0.trailing.equalToSuperview().inset(20.0)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-34.0)
+            $0.height.equalTo(56.0)
         }
     }
     
